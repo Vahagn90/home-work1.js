25f1a3972b35489fe6ff7df2553d350917cdd340
Vahagn90 <84335561+Vahagn90@users.noreply.github.com>
2021-05-24 11:17:28 +0400

    Add files via upload

---
+++ home worke1.js
@@ -0,0 +1,105 @@
+// xndir 1
+
+let Name=prompt('what is your name?', ' ') 
+let Surname=prompt('what is your surname?', ' ')
+result=(Name + Surname)
+alert(result)
+
+
+
+
+// xndir 2
+
+let a=14
+let b=7
+result = a > b
+
+let a=14
+let b=7
+
+result = a + b
+
+let a=14
+let b=7
+
+result = a - b
+
+let a=14
+let b=7
+
+result = a * b
+
+let a=14
+let b=7
+
+result = a / b
+
+let a=14
+let b=7
+
+result = a % b
+
+// xndir 3
+
+let sampleNumber = 78756,
+  lastDigit = sampleNumber % 10;
+console.log('The last digit of ', sampleNumber, ' is ', lastDigit);
+
+
+let sampleNumber = -122,
+  lastDigit = sampleNumber % 10;
+console.log('The last digit of ', sampleNumber, ' is ', lastDigit);
+
+
+let sampleNumber = 8,
+  lastDigit = sampleNumber % 10;
+console.log('The last digit of ', sampleNumber, ' is ', lastDigit);
+
+
+let sampleNumber = 0,
+  lastDigit = sampleNumber % 10;
+console.log('The last digit of ', sampleNumber, ' is ', lastDigit);
+
+
+
+// xndir 4
+
+let firstName = 'Vahagn'
+let age = 31
+
+alert('My name is: ' + firstName + ', I am :' + age)
+
+
+
+// xndir 5
+
+let a = 8
+let b = 12
+a = a + b
+b = a - b
+a = a -b
+
+let a = 8
+let b = 12
+a= a+b
+20
+b= a- b
+8
+a= a- b
+12
+
+let a = 10
+let b = 20
+a = a + b
+b = a - b
+a = a - b
+
+let a = 10
+let b = 20
+undefined
+a=a+b
+30
+b= a- b
+10
+a= a- b
+20
